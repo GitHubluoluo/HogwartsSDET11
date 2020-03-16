3f65b75761bc276e91b94a440a5558b7d26024d8
Yong <546885255@qq.com>
2020-03-16 22:48:31 +0800

    add appium 定位

--- ex_appium/test_xueqiu.py
+++ ex_appium/test_xueqiu.py
@@ -36,9 +36,10 @@ class TestXueqiu:
         # 如果进程在不用杀死 测试app
         caps["unicodeKeyboard"] = True  # 输入中文
         caps["resetKeyboard"] = True   # 恢复键盘
+        caps["skipServerInstallation"] = True  # 跳过server 测试 框架 安装
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
         # 显式等待
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(20)
         # 隐试等待
         # agree_btn = (MobileBy.ID, "tv_agree")
         # WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(agree_btn))
@@ -66,6 +67,8 @@ class TestXueqiu:
         ali_price = self.driver.find_element(MobileBy.ID, "current_price").text
         assert float(ali_price) > 100
 
+    # appium 滑动操作
+    # appium文档 http://appium.io/docs/en/about-appium/intro/
     def test_scroll(self):
         # self.driver.get_window_size()
         mobile = self.driver.get_window_rect()
@@ -101,6 +104,13 @@ class TestXueqiu:
         self.driver.lock(5)
         self.driver.unlock()
 
+    # 课堂作业 通过祖爷爷 找叔叔
+    """
+    XPATH
+    更多关于xpath 语法请看文档  
+    https://www.w3school.com.cn/xpath/index.asp
+    实例 https://blog.csdn.net/weixin_30418341/article/details/94961241
+    """
     def test_get_ali_hk_price(self):
         # 重置数据 为了速度快 可以打开此项 然后 开始点击的同意就不用再点击了
         # caps["noReset"] = True
@@ -108,16 +118,67 @@ class TestXueqiu:
         self.driver.find_element(MobileBy.ID, "tv_search").click()
         self.driver.find_element(MobileBy.ID, "search_input_text").send_keys("阿里巴巴")
         self.driver.find_element(MobileBy.ID, "name").click()
-        ali_price = self.driver.find_element(MobileBy.XPATH, "//*[@text='09988']/../TextView[2]").text
-        print(ali_price)
-        # assert float(ali_price) > 100
+        # stock = "//*[@resource-id='com.xueqiu.android:id/title_container']//*[@text='股票']"
+        stock = "//*[contains(@resource-id, 'title_container')]//*[@text='股票']"
+        self.driver.find_element(MobileBy.XPATH, stock).click()
+        # ele_xpath = "//*[@text='09988']/parent::android.widget.LinearLayout/../../android.widget.LinearLayout[2]"
+        # ele_xpath = "//*[@text='09988']/../../../android.widget.LinearLayout[2]/android.widget.TextView[1]"
+        ele_xpath = "//*[@text='09988']/../../..//*[contains(@resource-id,'current_price')]"
 
+        ali_price = self.driver.find_element(MobileBy.XPATH, ele_xpath).text
+        print(ali_price)
+        assert float(ali_price) > 100
+
+    # UiAutomator Selector
+    # 滚动到 某个元素
+    """
+    框架 底层 提供 UiAutomator Selector 
+    文档 https://developer.android.com/reference/android/support/test/uiautomator/UiSelector.html
+    只适用于安卓
+    文档 http://appium.io/docs/en/writing-running-appium/android/uiautomator-uiselector/index.html
+    事例 https://www.cnblogs.com/cnkemi/p/9185253.html
+    1.ID resourceId
+        loc_id = 'new UiSelector().resourceId("com.baidu.yuedu:id/webbooktitle")'
+        driver.find_element_by_android_uiautomator(loc_id).click()
+    2.文本 text
+        loc_text = 'new UiSelector().text("图书")'
+        driver.find_element_by_android_uiautomator(loc_text).click()
+    3.类 className复数定位
+        loc_class = 'new UiSelector().className("android.widget.TextView")'
+        driver.find_elements_by_android_uiautomator(loc_class)[2].click()
+    4.description 也是用contenet-des属性定位
+        new UiSelector().description("contenet-des属性")
+    5.组合定位
+        1).id与text属性组合
+            #id+text
+            id_text = 'resourceId("com.baidu.yuedu:id/webbooktitle").text("小说")'
+            driver.find_element_by_android_uiautomator(id_text).click()
+        2).class与text属性组合
+            #class+text
+            class_text = 'className("android.widget.TextView").text("图书")'
+            driver.find_element_by_android_uiautomator(class_text).click()    
+    """
+
+    def test_ui_selector(self):
+
+        """
+        UiScrollable 如果某个控件可以滚动 定位到控件 
+        控件的scrollable(true)属性为真
+        调用视图方法 scrollIntoView 传入元素
+        UiSelector 定位元素的方法
+        """
+        sel_str = 'new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text("1小时前").instance(0));'
+        scroll_to_element = (MobileBy.ANDROID_UIAUTOMATOR, sel_str)
+        self.driver.find_element(*scroll_to_element).click()
+
+    # 获取当前 页面资源
     def test_page_source(self):
         # 获取当前页资源
         xml = self.driver.page_source
         # todo：添加一个写入文件的功能
         self.log_xml(xml)
 
+    # 写入文档
     def log_xml(self, page_xlm):
         now = datetime.now()
         with open(now.strftime('H%M%S')+'.xml', 'a') as f:
--- notebook/lenrn_appium/appium_doc.py
+++ notebook/lenrn_appium/appium_doc.py
@@ -7,6 +7,7 @@
 @File : appium_doc.py 
 @Software: PyCharm
 """
+
 # 打开投屏 工具
 # http://zuimeia.com/app/6771/?platform=2
 """
@@ -54,7 +55,9 @@ scrcpy -S
 # 使用 appium servse
 """
 运行 appium 使用 -g 参数追加 日志 
+
 $ appium -g 'C:\Users\Yong\PycharmProjects\HogwartsSDET11\ex_appium\appium.log'
+
 [Appium] Welcome to Appium v1.16.0
 [Appium] Non-default server args:
 [Appium]   logFile: C:\Users\Yong\PycharmProjects\HogwartsSDET11\ex_appium\appium.log
@@ -65,6 +68,8 @@ $ appium -g 'C:\Users\Yong\PycharmProjects\HogwartsSDET11\ex_appium\appium.log'
 查看 分析 adb命令 用途
 """
 
+# 定位
+# 事例 https://blog.csdn.net/ouyanggengcheng/article/details/88558224
 # 可以使用 控件定位 工具 uiautomatorviewer
 # 在sdk  Tools  bin 下面
 # Ps： uiautomatorviewer
@@ -91,6 +96,71 @@ resource-id	com.xueqiu.android:id/title_text
 XPATH
 更多关于xpath 语法请看文档  
 https://www.w3school.com.cn/xpath/index.asp
+实例 https://blog.csdn.net/weixin_30418341/article/details/94961241
+
+框架 底层 提供 UiAutomator Selector 
+文档 https://developer.android.com/reference/android/support/test/uiautomator/UiSelector.html
+只适用于安卓
+官方 实例1  http://appium.io/docs/en/writing-running-appium/android/uiautomator-uiselector/index.html
+
+实例2  https://www.cnblogs.com/cnkemi/p/9185253.html
+元素定位方式有多种，Android也有自身独有的定位方式。下面就单独介绍其基于uiautomator定位元素的方法：
+基本语法：
+driver.find_element_by_android_uiautomator(xx)
+1.文本定位
+    1).通过text文本定位语法
+    new UiSelector().text("text文本")
+    #text
+    loc_text = 'new UiSelector().text("图书")'
+    driver.find_element_by_android_uiautomator(loc_text).click()
+    2).如果文本比较长，可以用textContains模糊匹配
+    new UiSelector().textContains("包含text文本")
+    # textContains
+    loc_textContains = 'new UiSelector().textContains("图")'
+    driver.find_element_by_android_uiautomator(loc_textContains).click()
+    3).同样可以用textStartsWith是以某个文本开头来匹配
+    new UiSelector().textStartsWith("以text文本开头")
+    #textStartsWith
+    loc_textStart = 'new UiSelector().textStartsWith("图")'
+    driver.find_element_by_android_uiautomator(loc_textStart).click()
+    4).也可以用正则表达式textMatches匹配
+    new UiSelector().textMatches("正则表达式")
+2.resourceId 与by_id一样
+    new UiSelector().resourceId("id")
+    #resourceId
+    loc_id = 'new UiSelector().resourceId("com.baidu.yuedu:id/webbooktitle")'
+    driver.find_element_by_android_uiautomator(loc_id).click()
+3.className
+    页面上的class属性一般不唯一，多半用在复数定位时候。此时定位相应下标
+    new UiSelector().className("className")
+    #className复数定位
+    loc_class = 'new UiSelector().className("android.widget.TextView")'
+    driver.find_elements_by_android_uiautomator(loc_class)[2].click()
+4.description
+    也是用contenet-des属性定位
+    new UiSelector().description("contenet-des属性")
+5.组合定位
+    1).id与text属性组合
+    #id+text
+    id_text = 'resourceId("com.baidu.yuedu:id/webbooktitle").text("小说")'
+    driver.find_element_by_android_uiautomator(id_text).click()
+    2).class与text属性组合
+    #class+text
+    class_text = 'className("android.widget.TextView").text("图书")'
+    driver.find_element_by_android_uiautomator(class_text).click()
+6.关系定位
+    1).父子定位childSelector
+    有时候不能直接定位某个元素，但是它的父元素很好定位，这时候就先定位父元素，通过父元素找儿子
+    #父子关系childSelector
+    son = 'resourceId("com.baidu.yuedu:id/rl_tabs").childSelector(text("小说"))'
+    driver.find_element_by_android_uiautomator(son).click()
+     2).兄弟定位fromParent
+    有时候父元素不好定位，但是跟他相邻的兄弟元素很好定位，这时候就可以通过兄弟元素，找到同一父级元素下的子元素
+    #兄弟关系fromParent
+    brother = 'resourceId("com.baidu.yuedu:id/lefttitle").fromParent(text("图书"))'
+    driver.find_element_by_android_uiautomator(brother).click()
+
+
 
 绝对定位 不推荐 路径太长
 
@@ -107,6 +177,19 @@ accessibilityId ：content-desc
 
 不推荐  ：name  无用  class name 也不推荐 
 
+可以使用 self.driver.page_source 获取资源 
+然后使用 idea 工具 XpathView 测试 
+
+
+def test_page_source(self):
+       # 获取当前页资源
+       xml = self.driver.page_source
+       # todo：添加一个写入文件的功能
+       self.log_xml(xml)
+   def log_xml(self, page_xlm):
+       now = datetime.now()
+       with open(now.strftime('H%M%S')+'.xml', 'a') as f:
+           f.write(page_xlm)
 
 """
 
